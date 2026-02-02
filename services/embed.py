25bcdff08a5d6ee74e977279b288ed5a02d02283
Dishita Sood <149025991+dishitasood@users.noreply.github.com>
2026-02-02 16:47:26 -0500

    Added chromadb

--- services/embed.py
+++ services/embed.py
@@ -1,3 +1,12 @@
+import chromadb
 
+client = chromadb.PersistentClient(path="./db")
+collection = client.get_or_create_collection("docs")
 
-# Recursive Character Text Splitter
+with open("cleaned_earnings_call.txt", "r") as f:
+    text = f.read()
+
+collection.add(documents=[text], ids=["Fin"])
+
+print("Embedding stored in Chroma")
+
