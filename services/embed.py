6bf0cb882010db57020816dfeb7e9f09509842b8
Dishita Sood <149025991+dishitasood@users.noreply.github.com>
2026-02-03 19:59:14 -0500

    Created chromadb embeddings

--- services/embed.py
+++ services/embed.py
@@ -1,12 +1,47 @@
 import chromadb
+import json
+import ollama
+
+with open("services/chunk_earnings_call.json", "r") as f:
+    chunks = json.load(f)
+
 
 client = chromadb.PersistentClient(path="./db")
-collection = client.get_or_create_collection("docs")
+existing = [c.name for c in client.list_collections()]
+if "earnings_call" in existing:
+    client.delete_collection(name="earnings_call")
 
-with open("cleaned_earnings_call.txt", "r") as f:
-    text = f.read()
+collection = client.get_or_create_collection(name="earnings_call")
 
-collection.add(documents=[text], ids=["Fin"])
+def embed_text(text):
+    response = ollama.embeddings(
+        model="nomic-embed-text",
+        prompt=text
+    )
+    return response["embedding"]
 
-print("Embedding stored in Chroma")
+assert len(embed_text("test")) == 768
 
+documents = []
+embeddings = []
+metadatas = []
+ids = []
+
+for chunk in chunks:
+    documents.append(chunk["text"])
+    embeddings.append(embed_text(chunk["text"]))
+    metadatas.append(chunk["metadata"])
+    ids.append(str(chunk["chunk_id"]))
+
+collection.add(
+    documents=documents,
+    embeddings=embeddings,
+    metadatas=metadatas,
+    ids=ids
+)
+
+# Quick Example
+results = collection.query(
+    query_texts=["What drove operating income growth in Q1 2024?"],
+    n_results=3
+)
